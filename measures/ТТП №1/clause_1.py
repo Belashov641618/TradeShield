b236d3ccc650e796028315a3c09290ac43bccb30
Belashov641618 <uclaptrap@gmail.com>
2025-10-19 01:45:40 +0300

    Написано второе условие

--- measures/ТТП №1/clause_1.py
+++ measures/ТТП №1/clause_1.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from postgres import declarations as tables
 from typing import Union
 from datetime import datetime, timezone
-__description__ = "Применяемая ставка таможенного тарифа ЕТТ ЕАЭС меньше предельной ставки в отношении кода, установленной обязательствами России в ВТО."
+__description__ = "Применяемая ставка таможенного тарифа ЕТТ ЕАЭС меньше предельной ставки в отношении кода, установленной обязательствами России в ВТО"
 
 def check(session:Session, code:int) -> dict[str,Union[str,float,list]]:
     good = tables.Goods.by_code(session, code)
@@ -13,7 +13,9 @@ def check(session:Session, code:int) -> dict[str,Union[str,float,list]]:
     vto_duty, status = good.get_metric("vto_duty")
     if vto_duty is None: return {"status": status}
 
-    degree = (vto_duty.value-current_duty.value)/good.vto_duty
+    if vto_duty.value <= current_duty.value:
+        degree = -1.0
+    else: degree = (vto_duty.value-current_duty.value)/good.vto_duty
     degree = max(min(degree,1.0),-1.0)
 
     now = datetime.now(timezone.utc)
---
+++ measures/ТТП №1/clause_2.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from postgres import declarations as tables
+from typing import Union
+from datetime import datetime, timezone
+__description__ = "Доля импорта из недружественных стран по перечню распоряжения № 430-р составляет менее 30% по состоянию на последнее полугодие или полный календарный год, доля снижается в сравнении с аналогичным предыдущим периодом"
+
+def check(session:Session, code:int) -> dict[str,Union[str,float,list]]:
+    good = tables.Goods.by_code(session, code)
+    if not good: return dict(status="Товар - не найден")
+    now = datetime.now(timezone.utc)
+
+    unfriendly_import_value = 0.
+    links = []
+    errors = []
+    for country in session.query(tables.Countries).filter_by(unfriendly=True).all():
+        import_value, status = good.get_metric("import_value", country=country.iso2)
+        if import_value is None: errors.append(status)
+        unfriendly_import_value += import_value.value
+        links.append((import_value.metric.full_name, import_value.source, (now-import_value.updated).total_seconds()))
+    if not links: return {"status":errors[0]}
+
+    total_import_value, status = good.get_metric("total_import_value")
+    if total_import_value is None: return {"status":status}
+    links.append((total_import_value.metric.full_name, total_import_value.source, (now-total_import_value.updated).total_seconds()))
+
+    degree = 1.0 if unfriendly_import_value/total_import_value < 0.3 else -1.0
+    degree = max(min(degree,1.0),-1.0)
+
+    return {
+        "status" : "OK",
+        "degree" : degree,
+        "links" : links
+    }
