0dce5ea744adf76265b6d81807d720a091e73944
Belashov641618 <uclaptrap@gmail.com>
2025-10-18 21:41:55 +0300

    Добавлена проверка акутальности метрик через Git

--- workers/data_extractor/data_extractor.py
+++ workers/data_extractor/data_extractor.py
@@ -4,7 +4,10 @@ import redis
 from fastapi import FastAPI
 from celery import Celery
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from postgres.declarations import Base
+from utilities import gitpull
 
 REDIS_HOST          = str(os.getenv("REDIS_HOST",           "localhost"))
 REDIS_PORT          = int(os.getenv("REDIS_PORT",           6379))
@@ -18,9 +21,17 @@ POSTGRES_PASSWORD   = str(os.getenv("POSTGRES_PASSWORD",    "password"))
 app = FastAPI(title="TradeShield")
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 celery = Celery("tasks", broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0", backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/1")
+
 engine = create_engine(f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 @celery.task(bind=True)
-def data_extractor(self, code:str, name:str, inn:str):
-    raise NotImplementedError
+def data_extractor(self, code:str, name:str, inn:str) -> list[tuple[str,str,dict[str,tuple[float,str]]]]:
+    gitpull(__file__)
+
+
+
+
