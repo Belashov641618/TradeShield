b81cb864e2210c0bebb16a6f6a06c0b8fc4e45d6
Belashov641618 <uclaptrap@gmail.com>
2025-10-19 02:12:12 +0300

    Исправление ошибки

--- measures/ТТП №1/clause_2.py
+++ measures/ТТП №1/clause_2.py
@@ -8,18 +8,18 @@ def check(session:Session, code:int) -> dict[str,Union[str,float,list]]:
     good = tables.Goods.by_code(session, code)
     if not good: return dict(status="Товар - не найден")
     now = datetime.now(timezone.utc)
+    links = []
 
     unfriendly_import_value = 0.
-    links = []
     errors = []
     for country in session.query(tables.Countries).filter_by(unfriendly=True).all():
-        import_value, status = good.get_metric("import_value", country=country.iso2)
+        import_value, status = good.get_metric("import_value_year", country=country.iso2)
         if import_value is None: errors.append(status)
         unfriendly_import_value += import_value.value
         links.append((import_value.metric.full_name, import_value.source, (now-import_value.updated).total_seconds()))
     if not links: return {"status":errors[0]}
 
-    total_import_value, status = good.get_metric("total_import_value")
+    total_import_value, status = good.get_metric("total_import_value_year")
     if total_import_value is None: return {"status":status}
     links.append((total_import_value.metric.full_name, total_import_value.source, (now-total_import_value.updated).total_seconds()))
 
