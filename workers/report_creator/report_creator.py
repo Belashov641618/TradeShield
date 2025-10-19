4837f99476ab3fc445af1210fee2086eaaa04861
Belashov641618 <uclaptrap@gmail.com>
2025-10-19 07:18:11 +0300

    Сделана заглушка для report_creator.py

--- workers/report_creator/report_creator.py
+++ workers/report_creator/report_creator.py
@@ -1,10 +1,11 @@
 import os
 import redis
 
+from json import dumps
 from fastapi import FastAPI
 from celery import Celery
 from sqlalchemy import create_engine
-from typing import Optional
+from typing import Union
 
 
 REDIS_HOST          = str(os.getenv("REDIS_HOST",           "localhost"))
@@ -22,5 +23,13 @@ celery = Celery("tasks", broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0", backend=
 engine = create_engine(f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}")
 
 @celery.task(bind=True)
-def report_creator(self, actions:Optional[dict[str,dict[str,str]]]=None):
-    raise NotImplementedError
+def report_creator(self, actions:dict[str,dict[str,Union[str,dict[str,Union[str,float]],bool,list[tuple[str,str,float]]]]]):
+    from time import sleep
+    for i in range(100):
+        prog = dict(prog=float(i+1), eta=None, step="Процесс генерации")
+        redis_client.publish(f"task_{self.request.id}", dumps(prog))
+        self.update_state(state="PROGRESS", meta=prog)
+        sleep(0.25)
+    with open("default.pdf", "rb") as file:
+        data = file.read()
+    return data
